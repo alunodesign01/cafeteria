f97e11470b74766cf9603e4414f2ffa7a20ab24f
alunodesign01 <152926235+alunodesign01@users.noreply.github.com>
2024-01-30 19:05:50 -0300

    PRIMEIRO COMMIT

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname='localhost';
+$username='root';
+$passoword='';
+$database='cafeteria';
+$port='3307';
+
+$conexao = mysqli_connect($hostname, $username, $passoword, $database,$port);
+
+
+
+
+?>
